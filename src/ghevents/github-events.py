f7f0878325dd4f25c647727e031c74525508607a
Junhee Sim <junheesim1@gmail.com>
2026-09-23 18:02:24 -0400

    Complete lab 03 scripting

---
+++ src/ghevents/github-events.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import requests
+
+GHUSER = os.getenv('GITHUB_USER')
+url = f'https://api.github.com/users/{GHUSER}/events'
+
+def retrieve_events(url):
+    '''
+    Takes a single parameter, url, from which it retrieves events from the GitHub API.
+    '''
+    return json.loads(requests.get(url).text)
+
+def print_events(events, n=5):
+    '''
+    Loops over the first n events (default is 5) and prints the event type and the repo name.
+    '''
+    for x in events[:n]:
+        event = x['type'] + ' :: ' + x['repo']['name']
+        print(event)
+
+def main():
+    '''
+    Prints the GitHub user and the URL of the events
+    '''
+    print(GHUSER)
+    print(url)
+    events = retrieve_events(url)
+    print_events(events)
+
+
+if __name__ == "__main__":
+    main()
